919b1d4cb82fed54533ba78bf894fcb6cbacdce0
hemingxi <hemingxi93@gmail.com>
2023-10-18 16:34:28 +0000

    started on Board class to place stones

--- code/dlgo/goboard_slow.py
+++ code/dlgo/goboard_slow.py
@@ -53,5 +53,91 @@ class GoString():
         return isinstance(other, GoString) and \
         self.color ==other.color and \
         self.stones == other.stones and \
-        self.liberties == other.liberties 
+        self.liberties == other.liberties
+    
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {} # underscore is convention that this variable is protected and should not be accessed/modified outside the class
+        # I'm assuming grid is a dictionary mapping points to their GoStrings. When you call _grid.get(point) you get the string-of-stones that that point is attached to
+
+    def place_stone(self, player, point):
+        """
+        This is a docstring for my_method.
+
+        Args:
+            param1: Description of the first parameter.
+            param2: Description of the second parameter.
+
+        Returns:
+            Description of the return value (if any).
+        """
+        assert self.is_on_grid(point) # point exists on grid
+        assert self._grid.get(point) is None # check that there's not already a stone there. _grid.get() returns the string of the stone that's there, or None if it's empty
+        
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties =[]
+
+        for neighbor in point.neighbors(): # we defined this in our gotypes.py, point.neighbors returns the 4 points surrounding the input point
+            if not self.is_on_grid(neighbor):
+                continue # if the neighboring point you are checking is off the grid, then continue the loop no need to do anything else
+            
+            neighbor_string = self._grid.get(neighbor) # grid is a dict mapping points to their GoStrings, neighbor is Point type
+            if neighbor_string is None: # this is the GoString type we defined earlier
+                # if neighbor_string is None, that means it's not already part of a GoString, meaning it is empty
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+
+            new_string = GoString(player, [point], liberties)
+            for same_color_string in adjacent_same_color: # [?] finish this part of the code given what you know
+                # merge the strings 
+            for new_string_point in new_string.stones:
+                # merge the strings
+            for other_color_string in adjacent_opposite_color:
+                # reduce liberties
+            for other_color_string in adjacent_opposite_color:
+                # capture if their liberties are now at 0
+
+
+    def is_on_grid(self, point):
+        # the grid will go from 1 to num_rows (not 0 delimited)
+        return 1 <= point.row <= self.num_rows and \
+        1 <= point.col <= self.num_cols
+    
+    def get(self, point): # [?] I don't think this function is used above? it's using the dictionary get method
+        """
+        If a stone's been placed on that point, gets you the color of that stone
+
+        Args:
+            point: a point of class Point
+
+        Returns:
+            Returns the content of a point: a Player (color) if a stone is on the that point, otherwise None
+        """
+        string = self._grid.get(point)
+        if string is None: # this part is redundant but helps with clarity and complies with convention
+            return None
+        return string.color
+    
+    def get_go_string(self, point): # [?] this funciton isn't used yet
+        """
+        If a stone's been placed on that point, gets you string that stone is attached to
+
+        Args:
+            point: a point of class Point
+
+        Returns:
+            Returns the string that stone is a part of
+        """
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
     
--- code/dlgo/gotypes.py
+++ code/dlgo/gotypes.py
@@ -13,7 +13,7 @@ from collections import namedtuple
 
 class Point(namedtuple('Point', 'row col')):
     # create a namedtuple for better readability - so you can refer to points by point.row and point.column
-    # the class "Point" inherits from the namedtuple class, defined as 
+    # the class "Point" inherits from the namedtuple class, defined with the name of "Point", and the two items named as "row" and "col"
     def neighbors(self):
         return [
             Point(self.row - 1, self.col),
