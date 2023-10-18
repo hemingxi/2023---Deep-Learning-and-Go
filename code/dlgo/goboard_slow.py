4438dc61e1fa06a58a0d11226fd6c7d63c1b19d7
hemingxi <hemingxi93@gmail.com>
2023-10-17 20:11:21 -0700

    added GoString class to track connected stones

--- code/dlgo/goboard_slow.py
+++ code/dlgo/goboard_slow.py
@@ -23,3 +23,35 @@ class Move():
     @classmethod
     def resign(cls):
         return(Move(is_resign = True))
+    
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones) # this uses the data type and removes duplicates
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point) # self.liberties is a set
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones # union the two sets
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+    
+    @property
+    def num_liberties(self):
+        return(len(self.liberties)) # it's a set, so calling len on it is polymorphism
+    
+    def __eq__ (self, other):
+        return isinstance(other, GoString) and \
+        self.color ==other.color and \
+        self.stones == other.stones and \
+        self.liberties == other.liberties 
+    
