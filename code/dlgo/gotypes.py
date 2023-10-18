aa36fb03b329f03089a1d83eb54034eaa04aa9e3
hemingxi <hemingxi93@gmail.com>
2023-10-17 19:37:31 -0700

    init commit

---
+++ code/dlgo/gotypes.py
@@ -0,0 +1,24 @@
+import enum
+
+class Player(enum.Enum):
+    black = 1
+    white = 2
+
+    @property # allows you to access this attribute without parenthesis
+    def other(self):
+        return Player.black if self == Player.white else Player.white
+    
+
+from collections import namedtuple
+
+class Point(namedtuple('Point', 'row col')):
+    # create a namedtuple for better readability - so you can refer to points by point.row and point.column
+    # the class "Point" inherits from the namedtuple class, defined as 
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col), 
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)
+        ]
+    
