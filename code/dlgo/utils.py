f953539ff4e6710ee671b1e314996ccea0855a61
hemingxi <hemingxi93@gmail.com>
2023-10-27 16:08:26 +0000

    Copied goboard_fast and dependent modules from the official repo. Testing still needs to be done.

--- code/dlgo/utils.py
+++ code/dlgo/utils.py
@@ -1,5 +1,6 @@
 from dlgo import gotypes
 from dlgo.goboard import Move, Board
+import numpy as np
 
 COLS = 'ABCDEFGHJKLMNOPQRST'
 STONE_TO_CHAR = {
@@ -29,3 +30,18 @@ def print_board(board: Board) -> None:
     
     print(f"    {''.join(COLS[:board.num_cols])}")
                   
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones((board.num_rows, board.num_cols))
+
+    def get(self, row, col):
+        return self.move_ages[row, col]
+
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = -1
+
+    def add(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
